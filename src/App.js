96d0323e8cfb6a7b1e6a898906830892b2936d21
Bianfas <marrostudios@gmail.com>
2019-05-15 20:59:15 -0500

    Adding MaterialUI Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+
 // import logo from './logo.svg';
 // import './App.css';
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+  <div>
+    <Typography variant="h2" align="center">Hello Sexy</Typography>
+    <TextField type="text" variant="outlined" margin="normal" placeholder="add to do"></TextField>
+  </div>
+  );
 }
 
 export default App;
